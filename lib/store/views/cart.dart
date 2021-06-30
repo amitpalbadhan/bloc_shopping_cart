f3e6d3a34e5d96dce9556a871ac89d69424bfc28
Amitpal <amitpalsbadhan@gmail.com>
2021-06-30 11:59:26 -0400

    removed commented out code

--- lib/store/views/cart.dart
+++ lib/store/views/cart.dart
@@ -37,7 +37,7 @@ class _CartSectionState extends State<CartSection> {
       children: <Widget>[
         SizedBox(height: 10),
         Text(
-          'Cart Total: \$${getTotal()}',
+          'Total: \$' + getTotal().toStringAsFixed(2),
           style: TextStyle(fontSize: 20.0),
         ),
         SizedBox(height: 10),
@@ -90,60 +90,6 @@ class _CartSectionState extends State<CartSection> {
         ),
       ],
     );
-    /*Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        GridView.count(
-          crossAxisCount: 4,
-          children: BlocProvider.of<StoreBloc>(context)
-              .cart
-              .map((product) => Card(
-                    margin: EdgeInsets.all(32.0),
-                    child: SizedBox(
-                      width: 300,
-                      height: 300,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Image.network(
-                              product.image,
-                              height: 150,
-                              width: 150,
-                              fit: BoxFit.cover,
-                            ),
-                            Text(product.title),
-                            Text('\$${product.price}'),
-                            Text(product.description),
-                            TextButton(
-                              onPressed: () {
-                                BlocProvider.of<StoreBloc>(context).add(
-                                    RemoveProductFromCart(product: product));
-                              },
-                              child: Text('Remove From Cart'),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ))
-              .toList(),
-        ),
-        SizedBox(height: 8.0),
-        TextButton(
-          onPressed: () {
-            BlocProvider.of<StoreBloc>(context).add(ClearCart());
-          },
-          child: Text('Clear Cart'),
-        ),
-        SizedBox(height: 8.0),
-        Text('Total: \$' + getTotal().toStringAsFixed(2)),
-      ],
-    );
-    */
   }
 
   double getTotal() {
